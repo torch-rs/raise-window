650bd89919bd65607dac3217b4d417d2b89a5516
Patrick Chan <patrickchan1999@gmail.com>
2018-07-20 22:27:31 -0400

    Modify raise_app to switch to desktop that app window is in

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,23 @@ mod parsing;
 mod windows;
 mod conditions;
 
-use xcb::Connection;
+use xcb::{Atom, Connection, Window};
 use failure::{Error, err_msg};
+
+fn get_property(conn: &Connection, window: Window, atom: Atom) -> Option<Vec<u8>> {
+    let cookie = xcb::get_property(conn,
+                                   false,
+                                   window,
+                                   atom,
+                                   xcb::GET_PROPERTY_TYPE_ANY,
+                                   0,
+                                   u32::max_value());
+    if let Ok(reply) = cookie.get_reply() {
+        let value: &[u8] = reply.value();
+        return Some(value.iter().cloned().collect());
+    }
+    None
+}
 
 pub fn raise_app(app_name: String) -> Result<(), Error> {
     let condition = &format!("class = \"{}\"", app_name);
@@ -23,10 +38,28 @@ pub fn raise_app(app_name: String) -> Result<(), Error> {
     let screen = conn.get_setup().roots().nth(screen_num as usize).unwrap();
 
     match windows::find_matching_window(&conn, &screen, &cond)? {
-        Some(win) => windows::set_active_window(&conn, &screen, win)?,
+        Some(win) => {
+            let net_wm_desktop = windows::get_atom(&conn, "_NET_WM_DESKTOP")?;
+            if let Some(value) = get_property(&conn, win, net_wm_desktop) {
+                let desktop_index = value[0] as u32;
+                let data = xcb::ClientMessageData::from_data32([desktop_index,
+                                                                xcb::CURRENT_TIME,
+                                                                0,
+                                                                0,
+                                                                0]);
+                let net_current_desktop = windows::get_atom(&conn, "_NET_CURRENT_DESKTOP")?;
+                let ev = xcb::ClientMessageEvent::new(32, screen.root(), net_current_desktop, data);
+                xcb::send_event(&conn,
+                                false,
+                                screen.root(),
+                                xcb::EVENT_MASK_SUBSTRUCTURE_NOTIFY | xcb::EVENT_MASK_SUBSTRUCTURE_REDIRECT,
+                                &ev).request_check()?;
+                windows::set_active_window(&conn, &screen, win)?;
+                conn.flush();
+            }
+        },
         None => return Err(err_msg("No matching window found")),
     }
-    conn.flush();
 
     Ok(())
 }
@@ -38,7 +71,7 @@ mod tests {
 
     #[test]
     fn raise_app_test() {
-        assert!(raise_app(String::from("Firefox")).is_ok());
+        assert!(raise_app(String::from("Caprine")).is_ok());
     }
 
 }
